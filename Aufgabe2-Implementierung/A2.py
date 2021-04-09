0d973fecdf0edd580ca1a7e6aa84c09bda62b988
Eric Wolf <robo-eric@gmx.de>
2021-04-09 23:14:18 +0200

    replace MissingDataError in Candidates.bowls with additional solution

--- Aufgabe2-Implementierung/A2.py
+++ Aufgabe2-Implementierung/A2.py
@@ -4,6 +4,7 @@
 
 import sys
 import argparse
+from itertools import chain
 from typing import TextIO, Iterator, Tuple, List, Dict, Set, Iterable
 
 __version__ = "0.1"
@@ -36,10 +37,26 @@ argparser.add_argument(
 )
 
 
-class Candidates(Dict[str, Set[str]]):  # Set hat eine effiziente Schnittmengenoperation
-    """Repräsentation der Kandidaten"""
+class InvalidDataError(ValueError):
+    """Fehler, welcher bei invaliden Daten ausgelöst wird"""
+    __slots__ = ()
 
-    def add_skewer(self, fruits: Iterable[str], bowls: Iterable[str]) -> None:
+
+class MissingDataError(ValueError):
+    """Fehler, welcher bei unvollständigen Daten ausgelöst wird"""
+    __slots__ = ()
+
+
+class Candidates(Dict[str, Set[int]]):  # Set hat eine effiziente Schnittmengenoperation
+    """Repräsentation der Kandidaten"""
+    __slots__ = ("all_candidates",)
+
+    all_candidates: Set[int]
+
+    def __init__(self, all_candidates: Set[int]) -> None:
+        self.all_candidates = all_candidates
+
+    def add_skewer(self, fruits: Iterable[str], bowls: Iterable[int]) -> None:
         """füge die Daten eines Spießes hinzu"""
         for fruit in fruits:
             try:
@@ -49,7 +66,7 @@ class Candidates(Dict[str, Set[str]]):  # Set hat eine effiziente Schnittmengeno
             else:
                 current_candidates &= frozenset(bowls)  # entferne Kandidaten, die für den neuen Spieß nicht in frage kommen
 
-    def add_skewers(self, skewers: Iterator[Tuple[Iterable[str], Iterable[str]]]) -> None:
+    def add_skewers(self, skewers: Iterator[Tuple[Iterable[str], Iterable[int]]]) -> None:
         """füge die Daten mehrerer Spieße hinzu"""
         for fruits, bowls in skewers:
             self.add_skewer(fruits, bowls)
@@ -66,31 +83,33 @@ class Candidates(Dict[str, Set[str]]):  # Set hat eine effiziente Schnittmengeno
                 for other_fruit, other_bowls in filter(lambda item: item[0] != fruit, self.items()):
                     if bowl in other_bowls:       # entferne unmöglichen Kandidaten
                         if len(other_bowls) < 2:  # andere Frucht ist auch eindeutig zugeordnet
-                            raise ValueError(
+                            raise InvalidDataError(
                                 f"{fruit} und {other_fruit} werden beide eineindeutig Schüssel {bowl} zugeordnet"
                             )
                         other_bowls.remove(bowl)    # kann die Frucht eindeutig machen
                         removed_candidate = True
 
-    def bowls(self, wanted: Iterable[str]) -> Set[str]:
+    def bowls(self, wanted: Iterable[str]) -> Set[int]:
         """gebe die Schüsseln einer Menge von Früchten zurück"""
         solution = set()
+        # Kandidaten für alle Früchte welche nicht in Spießen vorkommen
+        unknown_candidates = self.all_candidates - set(chain(*self.values()))   # alle Kandidaten - alle bereits vergebenen Kandidaten
         for fruit in wanted:
             try:
                 solution |= self[fruit]     # füge die Kandidaten der Frucht der Lösung hinzu
-            except KeyError as e:
-                raise ValueError(f"Frucht {fruit} ist unbekannt") from e
+            except KeyError:
+                solution |= unknown_candidates  # füge die noch nicht vergeben Kandidaten der Lösung hinzu
         return solution
 
 
-def parse_input(file: TextIO) -> Tuple[List[str], Iterator[Tuple[List[str], List[str]]]]:
-    """parse die bereitgestellte Datei und gebe die Wunschsorten und alle Spieße mit Schüsseln zurück"""
-    _ = file.readline()                 # Anzahl der Früchte, nicht benötigt
+def parse_input(file: TextIO) -> Tuple[Set[int], List[str], Iterator[Tuple[List[str], List[int]]]]:
+    """parse die bereitgestellte Datei und gebe alle Schüsseln, die Wunschsorten und alle Spieße mit Schüsseln zurück"""
+    all_bowls = int(file.readline())    # Anzahl der Früchte
     wanted = file.readline().split()    # Wunschsorten
-    return wanted, parse_skewers(file, int(file.readline()))
+    return set(range(1, all_bowls + 1)), wanted, parse_skewers(file, int(file.readline()))
 
 
-def parse_skewers(file: TextIO, skewers: int) -> Iterator[Tuple[List[str], List[str]]]:
+def parse_skewers(file: TextIO, skewers: int) -> Iterator[Tuple[List[str], List[int]]]:
     """parse die bereitgestellte Datei und gebe alle Spieße mit Schüsseln zurück"""
     for skewer in range(skewers):
         bowls = file.readline().split()     # Schüsseln eines Spießes, durch Leerzeichen getrennt
@@ -99,13 +118,13 @@ def parse_skewers(file: TextIO, skewers: int) -> Iterator[Tuple[List[str], List[
             raise ValueError(
                 f"Anzahl der Früchte und Schüsseln bei Spieß {skewer} stimmen nicht überein"
             )
-        yield fruits, bowls
+        yield fruits, [int(bowl) for bowl in bowls]
 
 
 if __name__ == "__main__":
     args = argparser.parse_args()
-    wanted, skewers = parse_input(args.Datei)
-    candidates = Candidates()
+    all_bowls, wanted, skewers = parse_input(args.Datei)
+    candidates = Candidates(all_bowls)
     candidates.add_skewers(skewers)
     candidates.remove_impossible()
     if args.debug:
@@ -113,8 +132,8 @@ if __name__ == "__main__":
             print(f"{fruit}: {bowls}")
     solution = candidates.bowls(wanted)
     if len(solution) != len(wanted):    # Lösung enthält unerwünschte Früchte
-        raise ValueError(
-            f"Anzahl der Früchte ({len(wanted)}) stimmt nicht mit der der zu besuchenden Schüsseln ({len(solution)}) überein, "
+        raise MissingDataError(
+            f"es müssen mehr Schüsseln ({solution}) als gewünschte Früchte ({wanted}) besucht werden, "
             "es Fehlen weitere Daten"
         )
-    print("Zu besuchende Schüsseln:", ", ".join(solution))
+    print("Zu besuchende Schüsseln:", solution)
