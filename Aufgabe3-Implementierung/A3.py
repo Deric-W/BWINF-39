285daf61231acf1ad5aaccb47fa1e4602afe9965
Eric Wolf <robo-eric@gmx.de>
2021-04-18 22:52:50 +0200

    update docs and prototype

--- Aufgabe3-Implementierung/A3.py
+++ Aufgabe3-Implementierung/A3.py
@@ -4,7 +4,6 @@
 
 import sys
 from argparse import ArgumentParser, FileType
-from enum import Enum
 from itertools import combinations
 from typing import TextIO, Tuple, Iterator, Sequence, List
 
@@ -24,18 +23,18 @@ argparser.add_argument(
     default=3
 )
 argparser.add_argument(
+    "-b",
+    "--brute",
+    help="benutze die Brute-Force Implementierung",
+    action="store_true"
+)
+argparser.add_argument(
     "Datei",
     help="Datei mit Daten des Dorfes (beim Fehlen wird von der Standarteingabe gelesen)",
     type=FileType("r"),
     nargs="?",
     default=sys.stdin
 )
-
-
-class VoteResult(Enum):
-    """Ergebnis einer Wahl"""
-    FAILED = 0
-    APPROVED = 1
 
 
 def parse_input(file: TextIO) -> Tuple[int, Iterator[int]]:
@@ -60,26 +59,41 @@ class Village:
         """ermittle die Distanz zur nächsten Eisbude"""
         distances = []
         for stall in position:
-            distance1 = stall - house   # Distanz gegen den Uhrzeigersinn
+            distance1 = abs(stall - house)   # Distanz gegen den Uhrzeigersinn
             distance2 = abs(distance1 - self.adresses)   # Distanz im Uhrzeigersinn
             distances.append(distance1 if distance1 < distance2 else distance2)
         return min(distances)
 
-    def simulate_vote(self, position1: Sequence[int], position2: Sequence[int]) -> VoteResult:
+    def simulate_vote(self, position1: Sequence[int], position2: Sequence[int]) -> bool:
         """Simuliere eine Wahl zwischen zwei Positionen"""
         approved = 0    # Anzahl der Stimmen für position2
         for house in self.houses:
             if self.distance(house, position2) < self.distance(house, position1):
                 approved += 1
-        half = len(self.houses) / 2
-        if approved > half:
-            return VoteResult.APPROVED
-        else:
-            return VoteResult.FAILED
+        return approved > len(self.houses) / 2
 
     def positions(self, stalls: int) -> Iterator[Tuple[int, ...]]:
         """ermittle alle möglichen Positionierungen der Buden"""
-        return combinations(range(self.adresses), stalls)
+        return combinations(range(self.adresses), stalls)   # Adressen sind 0 bis Adressen - 1
+
+
+def main1(positions: List[Tuple[int, ...]], village: Village, lazy: bool) -> None:
+    """Brute-Force Implementierung"""
+    better_positions = [0] * len(positions)     # speichere für jede Positionierung die Anzahl an besseren Positionierungen
+    for index1, position1 in enumerate(positions):
+        for index2, position2 in enumerate(positions[index1 + 1:]):     # teste alle Kombinationen
+            if village.simulate_vote(position1, position2):         # erhöhe die Anzahl an besseren Positionierungen des Verlierers um 1
+                better_positions[index1] += 1
+            else:
+                better_positions[index2] += 1
+        if better_positions[index1] == 0:   # Positionierung ist stabil
+            print("stabile Position:", position1)
+            if lazy:
+                break
+
+
+def main2(positions: List[Tuple[int, ...]], village: Village, lazy: bool) -> None:
+    pass
 
 
 if __name__ == "__main__":
@@ -87,14 +101,7 @@ if __name__ == "__main__":
     adresses, houses = parse_input(args.Datei)
     village = Village(adresses, list(houses))
     positions = list(village.positions(args.stalls))
-    better_positions = [0] * len(positions)
-    for index1, position1 in enumerate(positions):
-        for index2, position2 in enumerate(positions[index1 + 1:]):
-            if village.simulate_vote(position1, position2) is VoteResult.APPROVED:
-                better_positions[index1] += 1
-            else:
-                better_positions[index2] += 1
-        if better_positions[index1] == 0:
-            print("stabile Position:", position1)
-            if args.lazy:
-                break
+    if args.brute:
+        main1(positions, village, args.lazy)
+    else:
+        main2(positions, village, args.lazy)
